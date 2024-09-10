07b8410ec3c29de6b8dd2dd8150c4dc3052350c6
Yi Hong-ju <to_hongju@pm.me>
2024-09-10 22:46:26 +0900

    Add Inter, Lusitana fonts
    Applied Inter as a global font.
    Also uncommented `AcmeLogo` component by adding the Lusitana font.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from "next/font/google"; // Primary font
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
